45c5981b0b78818726cff06c00b4c2d6c4d28522
leo2971998 <106453722+leo2971998@users.noreply.github.com>
2022-08-05 16:15:56 -0500

    Updated v3!

--- src/main/java/org/example/UpdateProcedures.java
+++ src/main/java/org/example/UpdateProcedures.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import oracle.ucp.proxy.annotation.Pre;
+
 import java.sql.*;
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -16,18 +18,19 @@ public class UpdateProcedures {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yy");
         Connection Con = null;
         CallableStatement Cstmt = null;
-        CallableStatement Cstmtdp = null;
+        PreparedStatement pstmt = null;
+        ResultSet resultSet = null;
         Scanner sc = new Scanner(System.in);
         //1. Specify the driver
         Class.forName("oracle.jdbc.driver.OracleDriver");
         //2. Specify URL(IP Address/ DB Name/ Username/ Password
         Con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "leo", "leo29798");
         //3. Create statement
-        Cstmtdp = Con.prepareCall("{call sp_display(?)}");
-        Cstmtdp.registerOutParameter(1, Types.VARCHAR, 25);
-        System.out.println(Cstmtdp.getString(1));
-        Cstmtdp.executeUpdate();
-        //Con.commit();
+        Con.setAutoCommit(false);
+        Con.commit();
+        pstmt = Con.prepareStatement("Select empno, ename, sal, job from emp");
+        resultSet = pstmt.executeQuery();
+        writeResultSet(resultSet);
         System.out.print("Enter the Employee Num to update:");
         int eno = sc.nextInt();
         sc.nextLine();
@@ -43,7 +46,29 @@ public class UpdateProcedures {
         // 4. Execute
         Cstmt.executeUpdate();
         System.out.println(Cstmt.getString(4));
+        System.out.println("After update: ");
+        pstmt = Con.prepareStatement("Select empno, ename, sal, job from emp");
+        resultSet = pstmt.executeQuery();
+        writeResultSet(resultSet);
         // 5. Close the connection
         Con.close();
+        resultSet.close();
+    }
+    public static void writeResultSet(ResultSet resultSet) throws SQLException {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yy");
+        DecimalFormat df = new DecimalFormat("0.00");
+        // ResultSet is initially before the first data set
+        System.out.println(String.format("%5s %20s %20s %20s", "EMPNO", "ENAME", "SAL", "JOB"));
+        while (resultSet.next()) {
+            // It is possible to get the columns via name
+            // also possible to get the columns via the column number
+            // which starts at 1
+            // e.g. resultSet.getSTring(2);
+            int Empno = resultSet.getInt("EMPNO");
+            String Ename = resultSet.getString("ENAME");
+            String Job = resultSet.getString("JOB");
+            double sal = resultSet.getDouble("SAL");
+            System.out.println(String.format("%5s %20s %20s %20s", Empno, Ename, df.format(sal), Job));
+        }
     }
 }
